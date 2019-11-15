59ee52c4ea60b84a73ebf0fa57c98c4b0e731671
Laszlo Terray <laszlo.terray@codecool.com>
2019-11-15 11:07:20 +0100

    Some explanations about action creators and reducers

--- src/store/actions/actionsAdd.js
+++ src/store/actions/actionsAdd.js
@@ -1,5 +1,8 @@
 import * as actionTypes from './actionTypes';
 
+// action creators should maybe format data (for example returned from a server)
+// but logic is maybe better to be placed into the reducers
+
 const syncIncrement = () => {
   return {
     type: actionTypes.INCREMENT
@@ -11,6 +14,8 @@ export const increment = () => {
   return (dispatch, getState) => {
     // we don't use the getState here but it can be useful for
     // sending data to the server call for example
+    // YET, it is a better practice to provide necessary data as
+    // parameters to the action creator if possible
     setTimeout(() => {
       dispatch(syncIncrement())
     }, 2000);
